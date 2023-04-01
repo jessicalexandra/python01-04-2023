3a33d8b9f7390c9cef504ab5c6461c8a6e126247
mamberroqui <ranchermessi@gmail.com>
2023-04-01 10:58:09 -0500

    se crea primer cambio

---
+++ tracional.py
@@ -0,0 +1,8 @@
+#declaracion de una funcion tradicional
+def saludar(nombre):
+    print(f'hola {nombre}')
+
+# llamar a la funcion 
+
+resultado=saludar("jessica")
+print(resultado)
---
+++ tradicional.py
@@ -0,0 +1,7 @@
+#declaracion de una funcion tradicional
+def saludar(nombre):
+    print(f'hola {nombre}')
+
+# llamar a la funcion 
+
+saludar("jessica")
